a285bc87b4076d32a07ec7d01713b449be262891
2826WaveRobotics <wavesoftware@waverobotics.com>
2026-02-14 17:12:57 -0600

    Update year from 2025 to 2026

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "SeasonTemplate-Imported";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "173535a39dbeaaf5135c1df211fb868995b565ed";
+  public static final String GIT_DATE = "2026-02-06 22:50:10 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-02-14 18:11:15 EST";
+  public static final long BUILD_UNIX_TIME = 1771110675815L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
